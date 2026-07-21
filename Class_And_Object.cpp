b04a006940c2b7995b94b08743b73baf913442f0
Sumit Haldar <haldarsumit006@gmail.com>
2026-07-21 21:39:12 +0530

    Initial commit

---
+++ Class_And_Object.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // Library for input and output operations
+
+int main() {
+    // std::cout prints the text to the screen
+    std::cout << "Hello, World!" << std::endl; 
+    
+    return 0; // Indicates the program finished successfully
+}
