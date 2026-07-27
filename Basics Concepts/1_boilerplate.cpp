409589b5d7f3dcce0be2a7b773cef69e09a9a01d
Sumit Haldar <haldarsumit006@gmail.com>
2026-07-27 22:54:35 +0530

    correction of nameing

---
+++ Basics Concepts/1_boilerplate.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // preprocessor directive to include the input-output stream library
+using namespace std;
+
+int main(){
+   
+    return 0;
+}
+/* Preprocessor directives in C++ are special commands that instruct the preprocessor 
+to modify the source code text before actual compilation begins*/
--- Basics Concepts/2_for_loop.cpp
+++ Basics Concepts/2_for_loop.cpp
@@ -7,3 +7,5 @@ int main () {
     }
     return 0;
 }
+ 
+// This is a simple for loop that prints "sumit haldar" 5 times.
